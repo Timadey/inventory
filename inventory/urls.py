12d166f32fb99a58bc09f200e88441c23cb70432
Timothy Adeleke <65812744+Timadey@users.noreply.github.com>
2024-06-24 01:26:36 +0000

    Make swagger ui application home url

--- inventory/urls.py
+++ inventory/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 
 urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='home-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
